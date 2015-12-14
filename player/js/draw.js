d3760b23759e98db2ed0a62b7fbd756b33492587
Zack Guard <zacharyguard@gmail.com>
2015-12-14 20:10:03 +0800

    Draw gridlines

--- player/js/draw.js
+++ player/js/draw.js
@@ -10,6 +10,7 @@ automaton.draw = function() {
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, canvWidth, canvHeight);
 
+    /* draw cells */
     for (i in automaton.cells) {
         var cell = automaton.cells[i];
 
@@ -21,5 +22,24 @@ automaton.draw = function() {
 
             ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
         }
+    }
+
+    /* draw gridlines */
+    for (var c = 0; c < automaton.size.width; c++) { // no pun intended
+        ctx.strokeStyle = "#9E9E9E"; // material grey 500
+
+        ctx.beginPath();
+        ctx.moveTo(c * cellSize, 0);
+        ctx.lineTo(c * cellSize, canvHeight);
+        ctx.stroke();
+    }
+
+    for (var r = 0; r < automaton.size.width; r++) {
+        ctx.strokeStyle = "#9E9E9E";
+
+        ctx.beginPath();
+        ctx.moveTo(0, r * cellSize);
+        ctx.lineTo(canvWidth, r * cellSize);
+        ctx.stroke();
     }
 };
