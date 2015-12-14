c0186d2b64b9b0ea54e93928e161734fdbaa6b0a
Zack Guard <zacharyguard@gmail.com>
2015-12-14 20:17:31 +0800

    Move main code from init to main

--- player/js/main.js
+++ player/js/main.js
@@ -1,3 +1,38 @@
+/* clear function */
+
+automaton.clear = function() {
+    for (i in automaton.cells) {
+        delete automaton.cells[i]; // left with an array of undefineds
+    }
+};
+
+/* controls */
+
+document.querySelector(".control--step").addEventListener("click", function() {
+    automaton.step();
+    automaton.draw();
+});
+
+document.querySelector(".control--random-fill").addEventListener("click", function() {
+    for (var i = 0; i < automaton.cells.length; i++) {
+        automaton.cells[i] = [false, true][Math.round(Math.random())];
+    }
+    automaton.draw();
+});
+
+document.querySelector(".control--script-input").addEventListener("change", function() {
+    var file = this.files[0];
+
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var result = this.result;
+        automaton.rawStepScript = result;
+    };
+    reader.readAsText(file);
+});
+
+/* let's go */
+
 automaton.size.width = JSA_DEFAULT_GRID_WIDTH;
 automaton.size.height = JSA_DEFAULT_GRID_HEIGHT;
 
